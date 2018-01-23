b6d03212be698cc4c2ab01a1a39becedb07d97f8
Kayla Cross <kac288@georgetown.edu>
2018-01-23 16:19:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(name){
+  var newArray = [...kittens, name]
+  return newArray
+}
+
+function prependKitten(name){
+  var newArray = [name,...kittens]
+  return newArray
+}
+
+function removeLastKitten(){
+  return kittens.splice(0, kittens.length -1)
+}
